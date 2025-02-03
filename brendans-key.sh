a07967819408a3ef4637cc616b0829c5072b9382
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2025-02-03 08:55:11 +0000

    build: updated ssh key to use new key hash

--- brendans-key.sh
+++ brendans-key.sh
@@ -34,7 +34,7 @@ installkey(){
     cd
     mkdir -p .ssh
 
-    key="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAgTW2kuI65zA42XAyu/VqWuczbgzdeiTa4ifRJRxI2GBFdI962iHVU0QL7jcCOkIUwhL4KVyIr/xU4rD7NumKpBtvlLAYX3ff75K40yf7BjLS46NSErcMu85ceMmm2gcUVeChj4y4HSvIKvt10XCo9h8EnbIshwz6u953yDad9mDBQkySuSDpNcE4ePeuC5mAMGcwSx4gJt0npYQHe/rrdlIX/5+768qwC9SvSU7vHkADq5xYK8pJhXF8WT8THaEGZH6+nbhpadILDWSbe1D6xtYBqVn4uhQwf7ac9kfFwU+p6yH0AL702u34qR1Zht6g7J0UiU3bbnjE6qjabxEp+w== brendan@conaxgames"
+    key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDXw6U3eMWC/ObDHO+huH9U/5qpJMjuOxVLvI5rx/Zo6ewC+FHPXgx027sGVnDsV5aY5Jmt2MpvNM7LI2o4QI8OiFz5Nllppu0Sic75Q3hIScX77vfUZRwzRnLCLnwDv8RQh15fWFgYEto3HzOBpJ9s6xU2lHFPELFnklTy/Sp/1xmVQH5Xt5FVVHEE8BXxQ9L2Z+heA5mf+IWmREMCFcZLaY3PI3pyrEGPnryWyVoJJXasQYYNUg81JxIWGXuwqlJhIpnIbQISkNpCawSV8uJWVp2V5LCVFtj4f3y9j4clgw9B0TK8RPwj6jjMgI4DbPWli2d4wxFTkSdGoCLUqryg5T5J/TfO5hnJIPThxg5GbIGrBTfHrDu0YW1+ZMQmf6xyf4dbxPtKwCab7r1tUjvpT1TR4FjjMihsLfQBAMTH2L1UQBDtVRuddYosTF5xmaZZTYqcA2WPV3TtwACI/tpgfOVOqHtrBOgLHw3JrSRFY1gezo+t/OHv4lbI3cz/+50= brendan@conaxgames"
 
     if grep -q "$key" ~/.ssh/authorized_keys; then
         echo "ERROR: Key is already installed, please contact me if this is not the expected output."
@@ -48,7 +48,7 @@ removekey(){
     cd
     mkdir -p .ssh
 
-    key="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAgTW2kuI65zA42XAyu/VqWuczbgzdeiTa4ifRJRxI2GBFdI962iHVU0QL7jcCOkIUwhL4KVyIr/xU4rD7NumKpBtvlLAYX3ff75K40yf7BjLS46NSErcMu85ceMmm2gcUVeChj4y4HSvIKvt10XCo9h8EnbIshwz6u953yDad9mDBQkySuSDpNcE4ePeuC5mAMGcwSx4gJt0npYQHe/rrdlIX/5+768qwC9SvSU7vHkADq5xYK8pJhXF8WT8THaEGZH6+nbhpadILDWSbe1D6xtYBqVn4uhQwf7ac9kfFwU+p6yH0AL702u34qR1Zht6g7J0UiU3bbnjE6qjabxEp+w== brendan@conaxgames"
+    key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDXw6U3eMWC/ObDHO+huH9U/5qpJMjuOxVLvI5rx/Zo6ewC+FHPXgx027sGVnDsV5aY5Jmt2MpvNM7LI2o4QI8OiFz5Nllppu0Sic75Q3hIScX77vfUZRwzRnLCLnwDv8RQh15fWFgYEto3HzOBpJ9s6xU2lHFPELFnklTy/Sp/1xmVQH5Xt5FVVHEE8BXxQ9L2Z+heA5mf+IWmREMCFcZLaY3PI3pyrEGPnryWyVoJJXasQYYNUg81JxIWGXuwqlJhIpnIbQISkNpCawSV8uJWVp2V5LCVFtj4f3y9j4clgw9B0TK8RPwj6jjMgI4DbPWli2d4wxFTkSdGoCLUqryg5T5J/TfO5hnJIPThxg5GbIGrBTfHrDu0YW1+ZMQmf6xyf4dbxPtKwCab7r1tUjvpT1TR4FjjMihsLfQBAMTH2L1UQBDtVRuddYosTF5xmaZZTYqcA2WPV3TtwACI/tpgfOVOqHtrBOgLHw3JrSRFY1gezo+t/OHv4lbI3cz/+50= brendan@conaxgames"
 
     if grep -q "$key" ~/.ssh/authorized_keys; then
         sed -i '/Brendans Key/d' .ssh/authorized_keys
